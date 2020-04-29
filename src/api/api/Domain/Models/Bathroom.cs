4e4a65885f3131aa98177014e13a804a9541a92a
Joey Fallone <j.fallone@ualberta.ca>
2020-04-28 19:11:43 -0600

    Add Model for Reviews

--- src/api/api/Domain/Models/Bathroom.cs
+++ src/api/api/Domain/Models/Bathroom.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace api.Domain.Models
 {
     public class Bathroom
     {
         public string Name { get; set; }
         public Location Location { get; set; }
+        
+        public IEnumerable<Review> Reviews { get; set; }
     }
 }
---
+++ src/api/api/Domain/Models/Review.cs
@@ -0,0 +1,28 @@
+namespace api.Domain.Models
+{
+    public class Review
+    {
+        private float _rating;
+        public float Rating {
+            get => _rating;
+            set
+            {
+                if (value < 0)
+                {
+                    _rating = 0;
+                }
+                else if (value > 5f)
+                {
+                    _rating = 5;
+                }
+                else
+                {
+                    _rating = value;
+                }
+            } }
+        
+        public string Title { get; set; }
+        
+        public string Description { get; set; }
+    }
+}
