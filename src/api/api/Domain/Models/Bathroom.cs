c5f71cc09163a1b1b1584db42fe2ff2196d40ad7
Joey Fallone <j.fallone@ualberta.ca>
2020-05-02 20:34:48 -0600

    Add Id parameter to bathroom model
    Some low hanging fruit today!

--- src/api/api/Domain/Models/Bathroom.cs
+++ src/api/api/Domain/Models/Bathroom.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 
 namespace api.Domain.Models
 {
     public class Bathroom
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         
         // assuming a 1 - 1 mapping between bathrooms and locations for now
